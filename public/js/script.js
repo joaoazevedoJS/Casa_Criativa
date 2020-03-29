2cd317c17ed7e72853cdfc74d4528e7720d30092
João Azevedo <59920486+joaoazevedoJS@users.noreply.github.com>
2020-03-28 21:42:08 -0300

    Add ideais.html

--- public/js/script.js
+++ public/js/script.js
@@ -1,14 +1,11 @@
-const button =  document.querySelectorAll('.btn-modal')
+const button = document.querySelectorAll('.btn-modal')
 
 for(let event of button) {
   event.addEventListener('click', () => {
     document.querySelector('div.modal')
       .classList.toggle('hide')
 
-    document.querySelector('section.intro')
-      .classList.toggle('hide')
-
-    document.querySelector('section.last-ideias')
+    document.querySelector('div.container')
       .classList.toggle('hide')
   })
 }
